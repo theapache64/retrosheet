a2caf9a112bd75fb98c3244ec70ed137ccc1cad3
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-05-03 19:52:33 +0900

    Reduce scope of `.use` to only converting rows into list

--- retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/utils/CsvConverter.kt
+++ retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/utils/CsvConverter.kt
@@ -15,48 +15,46 @@ object CsvConverter {
         csvData: String,
         isReturnTypeList: Boolean
     ): String? {
-        return csvReader.parse(csvData.reader()).use { csvParser ->
-            val items = mutableListOf<Map<String, Any?>>()
-
-            for (row in csvParser.asIterator()) {
-                val item = csvParser.header.associateWith { key ->
-                    val field = row.getField(key)
-                    @Suppress("IMPLICIT_CAST_TO_ANY")
-                    when {
-                        field == null -> null
-                        TypeIdentifier.isInteger(field) -> field.toLong()
-                        TypeIdentifier.isBoolean(field) -> field.toBoolean()
-                        TypeIdentifier.isDouble(field) -> field.toDouble()
-                        else -> field.takeUnless { it.isBlank() }
+        val items = csvReader.parse(csvData.reader()).use { csvParser ->
+            csvParser
+                .asIterator()
+                .asSequence()
+                .map { row ->
+                    csvParser.header.associateWith { key ->
+                        val field = row.getField(key)
+                        @Suppress("IMPLICIT_CAST_TO_ANY")
+                        when {
+                            field == null -> null
+                            TypeIdentifier.isInteger(field) -> field.toLong()
+                            TypeIdentifier.isBoolean(field) -> field.toBoolean()
+                            TypeIdentifier.isDouble(field) -> field.toDouble()
+                            else -> field.takeUnless { it.isBlank() }
+                        }
                     }
                 }
+                .toList()
+        }
 
-                items.add(item)
+        return when {
+            isReturnTypeList -> {
+                val type = Types.newParameterizedType(List::class.java, Map::class.java)
+                val adapter = MoshiUtils.moshi.adapter<List<Map<String, Any?>>>(type)
+                adapter.toJson(items)
             }
 
-            when {
-                isReturnTypeList -> {
-                    val type = Types.newParameterizedType(List::class.java, Map::class.java)
-                    val adapter = MoshiUtils.moshi.adapter<List<Map<String, Any?>>>(type)
-                    adapter.toJson(items)
-                }
-
-                items.isNotEmpty() -> {
-                    val type = Types.newParameterizedType(Map::class.java, String::class.java, Any::class.java)
-                    val adapter = MoshiUtils.moshi.adapter<Map<String, Any?>>(type)
-                    adapter.toJson(items.first())
-                }
-
-                else -> {
-                    null
-                }
+            items.isNotEmpty() -> {
+                val type = Types.newParameterizedType(Map::class.java, String::class.java, Any::class.java)
+                val adapter = MoshiUtils.moshi.adapter<Map<String, Any?>>(type)
+                adapter.toJson(items.first())
             }
+
+            else -> null
         }
     }
 
     private fun CsvParser.asIterator(): Iterator<CsvRow> = CsvRowIterator(this)
 
-    private class CsvRowIterator(private val csvParser: CsvParser): Iterator<CsvRow> {
+    private class CsvRowIterator(private val csvParser: CsvParser) : Iterator<CsvRow> {
         private var row = csvParser.nextRow()
 
         override fun hasNext(): Boolean {
