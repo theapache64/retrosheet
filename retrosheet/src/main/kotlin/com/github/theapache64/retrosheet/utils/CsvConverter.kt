63510b8b6520caf2782f372982101b32f15aae2d
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-05-03 19:45:30 +0900

    Simplify CSV parsing by wrapping CsvParser into an iterator

--- retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/utils/CsvConverter.kt
+++ retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/utils/CsvConverter.kt
@@ -1,61 +1,39 @@
 package com.github.theapache64.retrosheet.utils
 
 import com.squareup.moshi.Types
+import de.siegmar.fastcsv.reader.CsvParser
 import de.siegmar.fastcsv.reader.CsvReader
+import de.siegmar.fastcsv.reader.CsvRow
 
 /**
  * Created by theapache64 : Jul 22 Wed,2020 @ 00:05
  */
 object CsvConverter {
+    private val csvReader: CsvReader = CsvReader().apply { setContainsHeader(true) }
+
     fun convertCsvToJson(
         csvData: String,
         isReturnTypeList: Boolean
     ): String? {
-        return CsvReader().apply {
-            setContainsHeader(true)
-        }.parse(csvData.reader()).use {
+        return csvReader.parse(csvData.reader()).use { csvParser ->
+            val items = mutableListOf<Map<String, Any?>>()
 
-            // Parsing CSV
-            val items = mutableListOf<Map<String, Any?>>().apply {
-                // Loading headers first
-                while (true) {
-                    val row = it.nextRow() ?: break
-                    val item = mutableMapOf<String, Any?>().apply {
-                        for (header in it.header) {
-                            val field = row.getField(header)
-                            when {
-                                TypeIdentifier.isInteger(
-                                    field
-                                ) -> {
-                                    put(header, field.toLong())
-                                }
-
-                                TypeIdentifier.isBoolean(
-                                    field
-                                ) -> {
-                                    put(header, field!!.toBoolean())
-                                }
-
-                                TypeIdentifier.isDouble(
-                                    field
-                                ) -> {
-                                    put(header, field.toDouble())
-                                }
-
-                                else -> {
-                                    val finalValue = if (field.isNullOrBlank()) {
-                                        null
-                                    } else {
-                                        field
-                                    }
-                                    put(header, finalValue)
-                                }
-                            }
-                        }
+            for (row in csvParser.asIterator()) {
+                val item = csvParser.header.associateWith { key ->
+                    val field = row.getField(key)
+                    @Suppress("IMPLICIT_CAST_TO_ANY")
+                    when {
+                        field == null -> null
+                        TypeIdentifier.isInteger(field) -> field.toLong()
+                        TypeIdentifier.isBoolean(field) -> field.toBoolean()
+                        TypeIdentifier.isDouble(field) -> field.toDouble()
+                        else -> field.takeUnless { it.isBlank() }
                     }
-                    add(item)
                 }
+
+                items.add(item)
             }
+
             when {
                 isReturnTypeList -> {
                     val type = Types.newParameterizedType(List::class.java, Map::class.java)
@@ -73,6 +51,22 @@ object CsvConverter {
                     null
                 }
             }
+        }
+    }
+
+    private fun CsvParser.asIterator(): Iterator<CsvRow> = CsvRowIterator(this)
+
+    private class CsvRowIterator(private val csvParser: CsvParser): Iterator<CsvRow> {
+        private var row = csvParser.nextRow()
+
+        override fun hasNext(): Boolean {
+            return row != null
+        }
+
+        override fun next(): CsvRow {
+            val currentRow = row!!
+            row = csvParser.nextRow()
+            return currentRow
         }
     }
 }
