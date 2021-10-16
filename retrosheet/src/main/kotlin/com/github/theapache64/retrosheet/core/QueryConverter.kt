091b3c2dac1f56ddf53d385e28654388aebd80c9
theapache64 <theapache64@gmail.com>
2021-10-16 13:41:20 +0530

    ☑️ add tests for QueryConverterTest

--- retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/core/QueryConverter.kt
+++ retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/core/QueryConverter.kt
@@ -3,13 +3,21 @@ package com.github.theapache64.retrosheet.core
 import com.github.theapache64.retrosheet.utils.TypeIdentifier
 
 /**
- * Created by theapache64 : Jul 21 Tue,2020 @ 22:37
+ * To create final query needed for the public sheet API.
+ *
+ * @property smartQuery The query
+ * @property smartQueryMap field map
+ * @property paramMap value map
  */
 class QueryConverter(
     private val smartQuery: String,
     private val smartQueryMap: Map<String, String>,
     private val paramMap: Map<String, String>?
 ) {
+    /**
+     * To generate final query.
+     * @return [smartQuery] replaced with [smartQueryMap] (fields) and [paramMap] (values)
+     */
     fun convert(): String {
         var outputQuery = smartQuery
         // Replacing values
--- retrosheet/src/test/kotlin/com/github/theapache64/retrosheet/core/QueryConverterTest.kt
+++ retrosheet/src/test/kotlin/com/github/theapache64/retrosheet/core/QueryConverterTest.kt
@@ -1,13 +1,45 @@
 package com.github.theapache64.retrosheet.core
 
+import com.github.theapache64.expekt.should
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class QueryConverterTest {
 
     @Test
-    fun convert() {
+    fun `Converts both fields and values`() {
+        val input = "SELECT * WHERE foo = :param1 AND bar = :param2"
+        val map = mapOf(
+            "foo" to "a",
+            "bar" to "b",
+        )
+        val paramMap = mapOf(
+            "param1" to "value1",
+            "param2" to "value2"
+        )
+        val actualOutput = QueryConverter(input, map, paramMap).convert()
+        actualOutput.should.equal("SELECT * WHERE a = 'value1' AND b = 'value2'")
+    }
 
+    @Test
+    fun `Converts fields only`() {
+        val input = "SELECT * WHERE foo = 'arg1' AND bar = 'arg2'"
+        val map = mapOf(
+            "foo" to "a",
+            "bar" to "b",
+        )
+        val actualOutput = QueryConverter(input, map, emptyMap()).convert()
+        actualOutput.should.equal("SELECT * WHERE a = 'arg1' AND b = 'arg2'")
+    }
+
+
+    @Test
+    fun `Converts values only`() {
+        val input = "SELECT * WHERE foo = :arg1 AND bar = :arg2"
+        val paramMap = mapOf(
+            "arg1" to "a",
+            "arg2" to "b"
+        )
+        val actualOutput = QueryConverter(input, emptyMap(), paramMap).convert()
+        actualOutput.should.equal("SELECT * WHERE foo = 'a' AND bar = 'b'")
     }
 }
